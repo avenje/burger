74f7faf8ffa2e13df97182101f0e8c50e6b112f7
avenje <jeremyavenarius@gmail.com>
2017-10-30 19:01:07 -0600

    update mysql

--- database/schema.sql
+++ database/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOL DEFAULT false,
+    created_at TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
     
